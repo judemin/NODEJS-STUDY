5eb72de55a71ee9bd02a2758271a377092656f70
judemin <judemin2087@naver.com>
2023-12-05 16:14:04 +0900

    ADD : add db sequelizer

--- 8-nodebird/controllers/page.js
+++ 8-nodebird/controllers/page.js
@@ -1,3 +1,17 @@
-exports.renderProfile = (req, res, next) => { };
-exports.renderJoin = (req, res, next) => { };
-exports.renderMain = (req, res, next) => { };
+// router -> controller -> service
+// controller : know req, res
+// service : dont know req, res
+exports.renderProfile = (req, res, next) => {
+    res.render('profile', { title: 'My information - NodeBird' });
+};
+
+exports.renderJoin = (req, res, next) => {
+    res.render('join', { title: 'Sign Up - NodeBird' });
+};
+
+exports.renderMain = (req, res, next) => {
+    res.render('main', {
+        title: 'NodeBird',
+        twits: [],
+    });
+};
