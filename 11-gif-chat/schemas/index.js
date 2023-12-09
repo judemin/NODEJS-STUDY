930a457067115f9b740359c0aa2103d36e67091b
judemin <judemin2087@naver.com>
2023-12-09 20:48:47 +0900

    ADD : add nodebird production model

--- 11-gif-chat/schemas/index.js
+++ 11-gif-chat/schemas/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const { MONGO_ID, MONGO_PASSWORD, NODE_ENV } = process.env;
-const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@localhost:27017/admin`;
-console.log(MONGO_URL);
+const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@127.0.0.1:27017/admin`;
 const connect = () => {
   if (NODE_ENV !== 'production') {
     mongoose.set('debug', true);
   }
+  console.log(MONGO_URL);
   mongoose.connect(MONGO_URL, {
     dbName: 'gifchat',
     useNewUrlParser: true,
